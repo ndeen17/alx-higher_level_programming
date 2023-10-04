67d510e349c8bf37d178c35b6e9874459c74fcda
ndeen17 <nurudeeny17@gmail.com>
2023-10-03 19:21:01 -0700

    this is python

---
+++ 10-check_cycle.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if the linked list contain a loop
+ * @list: link list to be searched
+ * Return: 1 0
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *slow = list;
+	listint_t *fast = list;
+
+	if (!list)
+		return (0);
+
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (1);
+	}
+
+	return (0);
+}
+
