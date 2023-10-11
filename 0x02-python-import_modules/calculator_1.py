6828bb64a0916fb0235a9a58b7bbda5b0d54d7d6
ndeen17 <nurudeeny17@gmail.com>
2023-10-10 17:37:38 -0700

    python

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+def add(a, b):
+    """this is the addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
+
+
+def sub(a, b):
+    """this is the subtraction function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a - b
+    """
+    return (a - b)
+
+
+def mul(a, b):
+    """this is my multiplication function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a * b
+    """
+    return (a * b)
+
+
+def div(a, b):
+    """this is my division function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a / b
+    """
+    return int(a / b)
---
+++ 0x02-python-import_modules/variable_load_5.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+a = 98
+"""this is a Simple variable
+"""
